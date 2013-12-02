d3b34dae78fb31c0f6b10e44b0174c0f82b91e56
Felix Hamann <nvri@dreadworks.de>
2013-12-02 18:31:20 +0100

    Played with the worlds elements parameters to find a suitable config. Added additional stats and controls.

--- prototypes/tree/tree.js
+++ prototypes/tree/tree.js
@@ -9,8 +9,12 @@ jQuery(function () {
 
 	var $canvas, canvas, ctx, bounds,
 		world, renderer, behaviours,
-		particles, forcefields;
+		particles, forcefields,
+		steps, $steps, paused, $cntrl;
 
+	// dom stuff
+	$steps = jQuery("#stats_steps");
+	$cntrl = jQuery("#cntrl");
 
 	// init physics
 	world = new Physics();
@@ -51,11 +55,11 @@ jQuery(function () {
 	behaviours.push(
 		Physics.behaviour('edge-collision-detection', {
 			aabb: bounds,
-			restitution: 1,
+			restitution: 0.5,
 			cof: 1
 		}),
 		Physics.behavior('body-impulse-response'),
-        Physics.behavior('newtonian', { strength: 0.001 }),
+        /*Physics.behavior('newtonian', { strength: 0.001 }),*/
         Physics.behavior('sweep-prune'),
         Physics.behavior('body-collision-detection', { checkAll: false })
 	);
@@ -66,24 +70,36 @@ jQuery(function () {
 		radius: 0,
 		x: canvas.width/2,
 		y: canvas.height/5,
-		mass: 10,
+		mass: 40,
 		fixed: true
 	}), Physics.body('circle', {
 		radius: 0,
 		x: canvas.width/2-50,
 		y: canvas.height/2,
-		mass: 30,
+		mass: 40,
+		fixed: true
+	}), Physics.body('circle', {
+		radius: 0,
+		x: canvas.width/2,
+		y: canvas.height/1.5,
+		mass: 40,
 		fixed: true
 	})];
 
 	particles = [];
-	_.times(200, function () {
+	_.times(100, function () {
 		particles.push(Physics.body('circle', {
-			x: _.random(canvas.width),
-			y: _.random(canvas.height),
-			mass: _.random(2, 5),
-			radius: _.random(4, 10),
-			restitution: 1
+			x: _.random(
+				canvas.width/5,
+				canvas.width/1.2
+			),
+			y: _.random(
+				canvas.height/5,
+				canvas.height/1.5
+			),
+			mass: 0.5,
+			radius: _.random(2,3),
+			restitution: 0.5
 		}));
 	});
 
@@ -97,17 +113,41 @@ jQuery(function () {
 		world.add(behaviour);
 	});
 
-	Physics.util.ticker.subscribe(function (time) {
+	steps = 0;
+	paused = false;
+
+	function render(time) {
+		steps += 1;
+
 		world.step(time);
 		world.render();
 
 		// draw other, static stuff
 		_.each(forcefields, function(f) {
 			ctx.beginPath();
-			ctx.arc(f.options.x, f.options.y, f.mass, 0, 3.14*2);
-			ctx.fillStyle = 'rgba(255, 0, 0, 0.3)';
+			ctx.arc(f.options.x, f.options.y, Math.log(f.mass), 0, Math.PI*2);
+			ctx.fillStyle = 'rgba(255, 0, 0, 0.8)';
 			ctx.fill();
 		});
+
+		// stats
+		$steps.text(steps);
+	}
+
+	Physics.util.ticker.subscribe(function (time) {
+		if (paused)
+			return;
+		render(time);
 	});
+
+	$cntrl.on('click', 'button.pause', function (ev) {
+		console.log('clickedi');
+		paused = paused? false : true;
+	});
+
+	$cntrl.on('click', 'button.go', function (ev) {
+		var val = $cntrl.find('input').val();
+		render(steps[val]);
+	})
 
 });
