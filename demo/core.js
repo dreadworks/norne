a07ba430c05e7ec1cc8d1e0120edc46c1d079aff
Felix Hamann <nvri@dreadworks.de>
2014-01-08 17:30:51 +0100

    updated the demo.

--- demo/core.js
+++ demo/core.js
@@ -1,16 +1,38 @@
 (function () {
+	'use strict';
 
 	/*
 	 *  called by norne
 	 */
 	function init(world) {
-		console.log('init called');
+		norne.log.trace('init called', world);
+
+		var lane = norne.lane.create({
+			dist: 30
+		});
+
+		lane.addPoint({
+			x: 300,
+			y: 50
+		}, {
+			x: 100,
+			y: 0
+		}, {
+			x: 800,
+			y: 20
+		}, {
+			x: 1200,
+			y: -20
+		});
+
+		world.addLane(lane);
+		world.create();
 	}
 
 
 	$(function () {
 		norne({
-			canvas: $('#norne')
+			canvas: $('#norne')[0]
 		}, init);
 	});
 
