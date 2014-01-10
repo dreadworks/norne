c7282377a868b38fcb96936beb8c916620d294e0
Felix Hamann <nvri@dreadworks.de>
2014-01-10 17:45:45 +0100

    finished first implementation and tests for norne.obj

--- src/util/obj.js
+++ src/util/obj.js
@@ -1,34 +1,99 @@
-norne.register('obj', {
+(function () {
 
-	objs: {},
+	var obj, slice;
 
-	exc: _({}).extend(norne.exception, {
-		name: 'norne.obj'
-	}),
+	slice = Array.prototype.slice;
 
+	obj = {
 
-	define: function (name) {
-		if (this.objs[name]) {
-			throw _({}).extend({
-				message: 'object with that name already exists'
+		proto: {},
+		extensions: [],
+
+		has: function (proto) {
+			this.proto = proto;
+			return this;
+		},
+
+		uses: function () {
+			this.extensions = slice.call(arguments);
+			return this;
+		},
+
+		as: function (object, constructor) {
+			this.base = _({}).extend(object);
+			this.constr = constructor;
+			return this;
+		},
+
+		create: function () {
+			var that = Object.create(this.proto);
+
+			// copy extensions
+			_(this.extensions).each(function (e) {
+				_(that).extend(e);
 			});
+
+			// extend with base object
+			_(that).extend(this.base);
+
+			// call constructor function
+			if (this.constr) {
+				this.constr.apply(that, arguments);
+			}
+
+			return that;
 		}
 
-		this.objs[name] = {};
-		return this;
-	},
+	};
 
-	use: function () {
-		return this;
-	},
 
-	extend: function () {
-		return this;
-	},
+	norne.register('obj', {
 
-	as: function (props) {
-		_(this).extend(props);
-		return this;
-	}
+		objs: {},
 
-});
+		exc: _({}).extend(norne.exception, {
+			name: 'norne.obj'
+		}),
+
+
+		get: function (name) {
+			return this.objs[name];
+		},
+
+
+		define: function (name) {
+
+			if (this.objs[name]) {
+				throw _({}).extend({
+					message: 'object with that name already exists'
+				});
+			}
+
+			this.objs[name] = Object.create(obj);
+			return this.objs[name];
+		},
+
+
+		erase: function (name) {
+			if (!this.objs[name]) {
+				throw _({}).extend({
+					message: 'object with that name does not exist'
+				});
+			}
+			delete this.objs[name];
+		},
+
+
+		create: function() {
+			var args, name, context;
+
+			args = Array.prototype.slice.call(arguments);
+			name = args.shift();
+			context = this.objs[name];
+
+			return context.create.apply(context, args);
+		}
+
+	});
+
+}());
---
+++ test/util.obj.spec.js
@@ -0,0 +1,149 @@
+describe('norne.obj', function () {
+
+	var o = norne.obj;
+
+	it('is accessible', function () {
+		expect(norne.obj).toBeDefined();
+	});
+
+
+	it('lets define and erase', function () {
+		var obj;
+
+		expect(norne.obj.define).toBeDefined();
+		obj = norne.obj.define('test');
+		expect(norne.obj.objs.test).toBeDefined();
+
+		expect(norne.obj.erase).toBeDefined();
+		norne.obj.erase('test');
+		expect(norne.obj.objs.test).not.toBeDefined();
+	});
+
+
+	it('can create', function () {
+		var obj, base, inst1;
+		expect(norne.obj.create).toBeDefined();
+
+		base = { x: 'x' };
+		obj = norne.obj.define('test').as(base);
+
+		inst1 = obj.create();
+
+		expect(inst1.x).toEqual(base.x);
+
+		inst1.x = 'something else';
+		expect(inst1.x).not.toEqual(base.x);
+
+		norne.obj.erase('test');
+	});
+
+
+	it('lets recieve', function () {
+		var obj;
+
+		expect(norne.obj.get).toBeDefined();
+		norne.obj.define('test');
+		obj = norne.obj.get('test');
+
+		expect(obj).toBeDefined();
+		norne.obj.erase('test');
+	});
+
+
+	it('creates independent instances', function () {
+		var obj, inst1, inst2;
+		obj = norne.obj.define('test').as({
+			x: 1
+		});
+
+		inst1 = obj.create();
+		inst2 = obj.create();
+
+		inst2.x = 2;
+
+		expect(inst1.x).toEqual(1);
+		expect(inst2.x).toEqual(2);
+
+		norne.obj.erase('test');
+	});
+
+
+	it('uses the constructor function', function () {
+		var obj, inst1, inst2;
+
+		obj = norne.obj.define('test').as({
+			x: 0
+		}, function (x) {
+			this.x = x;
+		});
+
+		inst1 = obj.create(1);
+		inst2 = obj.create(2);
+
+		expect(inst1.x).toEqual(1);
+		expect(inst2.x).toEqual(2);
+
+		norne.obj.erase('test');
+	});
+
+
+	it('can be invoked globally', function () {
+		var inst1;
+
+		norne.obj.define('test').as({
+			x: 0
+		}, function (x) {
+			this.x = x;
+		});
+
+		inst1 = norne.obj.create('test', 3);
+
+		expect(inst1.x).toEqual(3);
+		norne.obj.erase('test');
+	});
+
+
+	it('can be extended', function () {
+		var obj, base;
+		base = { x: 0 };
+
+		obj = norne.obj.define('test').uses(
+			base
+		).create();
+
+		expect(obj.x).toEqual(0);
+		base.x = 2;
+		expect(obj.x).toEqual(0);
+
+		norne.obj.erase('test');
+	});
+
+
+	it('may define a prototype', function () {
+		var proto1, obj, inst1, inst2;
+
+		proto1 = { proto1: 1 };
+
+		obj = norne.obj.define('test').has(
+			proto1
+		);
+
+		inst1 = obj.create();
+		inst2 = obj.create();
+
+		expect(inst1.proto1).toEqual(proto1.proto1);
+		expect(inst2.proto1).toEqual(proto1.proto1);
+
+		proto1.proto1 = 42;
+		expect(inst1.proto1).toEqual(proto1.proto1);
+		expect(inst2.proto1).toEqual(proto1.proto1);
+
+		norne.obj.erase('test');
+	});
+
+
+});
+
+
+
+
